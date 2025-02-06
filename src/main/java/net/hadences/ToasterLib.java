3425036419215e71029aa53331d051125a193d58
hadences <jaesunpark1213@gmail.com>
2025-02-05 18:56:01 -0600

    Added a helper function to easily send toast messages from the server to the client

--- src/main/java/net/hadences/ToasterLib.java
+++ src/main/java/net/hadences/ToasterLib.java
@@ -2,7 +2,11 @@ package net.hadences;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.hadences.network.ToasterPackets;
+import net.hadences.network.packets.S2C.SendToastMessageS2CPacket;
+import net.hadences.toast.ToastMessage;
+import net.minecraft.server.network.ServerPlayerEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,5 +20,14 @@ public class ToasterLib implements ModInitializer {
 //		DebugCommand.register();
 		ToasterPackets.registerPayloads();
 		ToasterPackets.registerC2SPackets();
+	}
+
+	/**
+	 * Helper function to send a toast message to a player
+	 * @param player The player to send the toast to
+	 * @param message The message to send
+	 */
+	public static void sendToast(ServerPlayerEntity player, ToastMessage message){
+		ServerPlayNetworking.send(player, new SendToastMessageS2CPacket(message));
 	}
 }
